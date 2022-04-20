556106233e9ee633f2900a4716f1eb47a6edaba0
sandwools <arktis@foxmail.com>
2022-04-20 14:34:04 +0800

    remake

--- Data_Structure/week5/myTree.cpp
+++ Data_Structure/week5/myTree.cpp
@@ -1,96 +1,128 @@
 #include"myTree.h"
 using namespace std;
 
+NodeTag ss;
 
-string pre;//保留前序遍历结果
-string ino;//保留中序遍历结果
+TreeNode *nownode=new TreeNode;
+TreeNode *Node=new TreeNode;
 
-// TreeNode *nownode=new TreeNode;
-// TreeNode *Node=new TreeNode;
+TreeNode::TreeNode()
+{
+	this->data='@';
+}
 
-void TreeNode::printNode(){
-	cout<<Node->data;//输出结点的值
+TreeNode::TreeNode
+(char A,TreeNode* B, TreeNode* C, NodeTag D,NodeTag E)
+{
+
+}
+
+TreeNode::~TreeNode()
+{
+
+}
+
+void TreeNode::printNode()
+{
+	cout<<Node->data;
 }
 
 
-MyTree::MyTree(){
-	num=0;//结点个数为0
+//...............................
+
+MyTree::MyTree()
+{
+	NodeNum=0;
 	flag=0;//不是线索二叉树
-	Tree=new TreeNode;//初始化根节点
-	Tree->data='@';//根节点为空
+
+	root=new TreeNode;//初始化根节点
+	root->data='@';//根节点为空
+
 	TreeNode *left=new TreeNode;
 	TreeNode *right=new TreeNode;
+
 	left->data='@';
 	right->data='@';
-	Tree->lchild=left;
-	Tree->rchild=right;
+	
+	root->leftChild=left;
+	root->rightChild=right;
 }
-MyTree::MyTree(const char *a){
+
+MyTree::MyTree(const char *a)
+{
 	int j=0;
-	while(a[j]!='\0'){//记录元素
+	while(a[j]!='\0')
+	{
 		node[j]=a[j];
 		j++;
 	}
 	flag=0;
-	Tree=new TreeNode;
+
+	root=new TreeNode;
+
 	TreeNode *left=new TreeNode;
 	TreeNode *right=new TreeNode;
-	Tree->lchild=left;
-	Tree->rchild=right;
+
+	root->leftChild=left;
+	root->rightChild=right;
+
 	left->data='@';//空
 	right->data='@';//空
-	Tree->data=a[0];//根节点赋初值
+	root->data=a[0];//根节点赋初值
+
 	int i=1;
+
 	TreeNode *p=new TreeNode;
-	p=Tree;
-	num=1;//结点个数
+	p=root;
+
+	NodeNum=1;
 	while(a[i]!='\0'){
-		while((p->lchild->data!='@'&&p->rchild->data!='@')||(p->lchild->tag==1&&p->rchild->tag==1)||(p->rchild->data!='@')){
+		while((p->leftChild->data!='@'&&p->rightChild->data!='@')||(p->leftChild->tag==1&&p->rightChild->tag==1)||(p->rightChild->data!='@')){
 			p=p->father;//回溯
 		}
 		if(a[i]!='@'){
-			if(p->lchild->data=='@'&&p->lchild->tag!=1){//结点插入左子树
+			if(p->leftChild->data=='@'&&p->leftChild->tag!=1){//结点插入左子树
 			    TreeNode *q=new TreeNode;
 			    q->data=a[i];
 				q->tag=0;
-    			p->lchild=q;
+    			p->leftChild=q;
 	    		TreeNode *left=new TreeNode;
 		    	TreeNode *right=new TreeNode;
     			left->data='@';
 	    		right->data='@';
 		    	q->father=p;
-    			q->lchild=left;
-	    		q->rchild=right;
+    			q->leftChild=left;
+	    		q->rightChild=right;
 		    	p=q;
 			    i++;
-    			num++;
+    			NodeNum++;
 	    		continue;
 		    }
-    		if(p->rchild->data=='@'&&p->rchild->tag!=1){//结点插入右子树
+    		if(p->rightChild->data=='@'&&p->rightChild->tag!=1){//结点插入右子树
 	    		TreeNode *q=new TreeNode;
 		    	q->data=a[i];
 				q->tag=0;
-			    p->rchild=q;
+			    p->rightChild=q;
     			TreeNode *left=new TreeNode;
 	    		TreeNode *right=new TreeNode;
 		    	left->data='@';
 			    right->data='@';
     			q->father=p;
-	    		q->lchild=left;
-		    	q->rchild=right;
+	    		q->leftChild=left;
+		    	q->rightChild=right;
 			    p=q;
     			i++;
-	    		num++;
+	    		NodeNum++;
 		    	continue;
 		    }
 		}
-		if(p->lchild->tag==0&&p->lchild->data=='@'){//左子树记为空
-			p->lchild->tag=1;
+		if(p->leftChild->tag==0&&p->leftChild->data=='@'){//左子树记为空
+			p->leftChild->tag=1;
 			i++;
 			continue;
 		}
 		else{//右子树记为空
-			p->rchild->tag=1;
+			p->rightChild->tag=1;
 			i++;
 			continue;
 		}
@@ -98,65 +130,65 @@ MyTree::MyTree(const char *a){
 }
 MyTree::MyTree(const MyTree &T){
 	flag=0;
-	Tree=new TreeNode;
+	root=new TreeNode;
 	TreeNode *left=new TreeNode;
 	TreeNode *right=new TreeNode;
-	Tree->lchild=left;
-	Tree->rchild=right;
+	root->leftChild=left;
+	root->rightChild=right;
 	left->data='@';//空
 	right->data='@';//空
-	Tree->data=T.node[0];//根节点赋初值
+	root->data=T.node[0];//根节点赋初值
 	int i=1;
 	TreeNode *p=new TreeNode;
-	p=Tree;
-	num=1;//结点个数
+	p=root;
+	NodeNum=1;//结点个数
 	while(T.node[i]!='\0'){
-		while((p->lchild->data!='@'&&p->rchild->data!='@')||(p->lchild->tag==1&&p->rchild->tag==1)||(p->rchild->data!='@')){
+		while((p->leftChild->data!='@'&&p->rightChild->data!='@')||(p->leftChild->tag==1&&p->rightChild->tag==1)||(p->rightChild->data!='@')){
 			p=p->father;//回溯
 		}
 		if(T.node[i]!='@'){
-			if(p->lchild->data=='@'&&p->lchild->tag!=1){//结点插入左子树
+			if(p->leftChild->data=='@'&&p->leftChild->tag!=1){//结点插入左子树
 			    TreeNode *q=new TreeNode;
 			    q->data=T.node[i];
 				q->tag=0;
-    			p->lchild=q;
+    			p->leftChild=q;
 	    		TreeNode *left=new TreeNode;
 		    	TreeNode *right=new TreeNode;
     			left->data='@';
 	    		right->data='@';
 		    	q->father=p;
-    			q->lchild=left;
-	    		q->rchild=right;
+    			q->leftChild=left;
+	    		q->rightChild=right;
 		    	p=q;
 			    i++;
-    			num++;
+    			NodeNum++;
 	    		continue;
 		    }
-    		if(p->rchild->data=='@'&&p->rchild->tag!=1){//结点插入右子树
+    		if(p->rightChild->data=='@'&&p->rightChild->tag!=1){//结点插入右子树
 	    		TreeNode *q=new TreeNode;
 		    	q->data=T.node[i];
 				q->tag=0;
-			    p->rchild=q;
+			    p->rightChild=q;
     			TreeNode *left=new TreeNode;
 	    		TreeNode *right=new TreeNode;
 		    	left->data='@';
 			    right->data='@';
     			q->father=p;
-	    		q->lchild=left;
-		    	q->rchild=right;
+	    		q->leftChild=left;
+		    	q->rightChild=right;
 			    p=q;
     			i++;
-	    		num++;
+	    		NodeNum++;
 		    	continue;
 		    }
 		}
-		if(p->lchild->tag==0&&p->lchild->data=='@'){//左子树记为空
-			p->lchild->tag=1;
+		if(p->leftChild->tag==0&&p->leftChild->data=='@'){//左子树记为空
+			p->leftChild->tag=1;
 			i++;
 			continue;
 		}
 		else{//右子树记为空
-			p->rchild->tag=1;
+			p->rightChild->tag=1;
 			i++;
 			continue;
 		}
@@ -164,35 +196,35 @@ MyTree::MyTree(const MyTree &T){
 }
 void MyTree::preOrderTraverse(){
 	stack<TreeNode*>s;
-	TreeNode *p=Tree;
+	TreeNode *p=root;
 	while(p->data!='@'||!s.empty()){
 		while(p->data!='@'){//向左遍历
 			cout<<p->data;
 			pre+=p->data;
 			s.push(p);
-			p=p->lchild;
+			p=p->leftChild;
 		}
 		if(!s.empty()){//回溯
 			p=s.top();
 			s.pop();
-			p=p->rchild;
+			p=p->rightChild;
 		}
 	}
 }
 void MyTree::inOrderTraverse(){
 	stack<TreeNode*>s;
-	TreeNode *p=Tree;
+	TreeNode *p=root;
 	while(p->data!='@'||!s.empty()){//向左遍历
 		while(p->data!='@'){
 			s.push(p);
-			p=p->lchild;
+			p=p->leftChild;
 		}
 		if(!s.empty()){//回溯
 			p=s.top();
 			cout<<p->data;
 			ino+=p->data;
 			s.pop();
-			p=p->rchild;
+			p=p->rightChild;
 		}
 	}
 }
@@ -203,39 +235,39 @@ void MyTree::post(int ileft1,int iright1,int ileft2,int iright2){
     cout<<pre[ileft1];//输出根节点
 }
 void MyTree::postOrderTraverse(){
-	post(0,num-1,0,num-1);//调用post函数
+	post(0,NodeNum-1,0,NodeNum-1);//调用post函数
 }
 int MyTree::countLeaf(){
 	stack<TreeNode*>s;
-	TreeNode *p=Tree;
+	TreeNode *p=root;
 	int count=0;
 	while(p->data!='@'||!s.empty()){
 		while(p->data!='@'){//向左遍历
-			if(p->lchild->tag==1&&p->rchild->tag==1){
+			if(p->leftChild->tag==1&&p->rightChild->tag==1){
 				count++;
 			}
 			s.push(p);
-			p=p->lchild;
+			p=p->leftChild;
 		}
 		if(!s.empty()){//回溯
 			p=s.top();
 			s.pop();
-			p=p->rchild;
+			p=p->rightChild;
 		}
 	}
 	return count;
 }
-int MyTree::height(const TreeNode *Tree){
+int MyTree::height(const TreeNode *root){
 	int m,n;
-	if(Tree->data=='@')return 0;
+	if(root->data=='@')return 0;
 	else{
-		m=height(Tree->lchild);//左子树高度
-		n=height(Tree->rchild);//右子树高度
+		m=height(root->leftChild);//左子树高度
+		n=height(root->rightChild);//右子树高度
 	}
 	return(m>n)?m+1:n+1;//取高的加1
 }
 int MyTree::countHeight(){
-	return height(Tree);//求高度
+	return height(root);//求高度
 }
 bool MyTree::isThreadedTree(){
 	if(flag)return true;//已经线索化
@@ -261,8 +293,14 @@ TreeNode& MyTree::nextNode(const TreeNode& n){
 	Node->data=ino[i+1];//将n的后继节点的值保存
 	return *Node;
 }
+
+
+
+
+//.......................
+
 HuffmanTree::HuffmanTree(const int& n, const int *a){
-	num=n;
+	NodeNum=n;
 	w=new int[n];
 	for(int i=0;i<n;i++){
 		w[i]=a[i];
@@ -291,25 +329,25 @@ HuffmanTree::HuffmanTree(const int& n, const int *a){
 					min2=array[j].weight;
 				}
 		//加入合成后的元素
-	    array[i].lchild=pos1;
-		array[i].rchild=pos2;
+	    array[i].leftChild=pos1;
+		array[i].rightChild=pos2;
 		array[i].weight=array[pos1].weight+array[pos2].weight;
 		array[pos1].parent=i;
 		array[pos2].parent=i;
 	}
 }
 void HuffmanTree::printHuffmanCodes(){
-	for(int i=0;i<num;i++){
-		cout<<w[num-i-1]<<":";//降序排列
+	for(int i=0;i<NodeNum;i++){
+		cout<<w[NodeNum-i-1]<<":";//降序排列
 		stack<int>s;
-		int pos=num-i-1;
+		int pos=NodeNum-i-1;
 		while(array[pos].parent!=-1){//取到根节点
 			int poss=pos;
 			pos=array[pos].parent;
-			if(array[pos].lchild==poss){//当前为左子树
+			if(array[pos].leftChild==poss){//当前为左子树
 				s.push(0);
 			}
-			else if(array[pos].rchild==poss){//当前为右子树
+			else if(array[pos].rightChild==poss){//当前为右子树
 				s.push(1);
 			}
 		}
--- Data_Structure/week5/myTree.h
+++ Data_Structure/week5/myTree.h
@@ -3,33 +3,46 @@
 #include<algorithm>
 #include<string.h>
 using namespace std;
-extern string pre;//保留前序遍历结果
-extern string ino;//保留中序遍历结果
 
-class TreeNode{
-	public:
+typedef 
+enum
+{
+	Link, 
+	Thread
+} NodeTag;
+
+
+string pre;//保留前序遍历结果
+string ino;//保留中序遍历结果
+
+class TreeNode
+{
+public:
 	int lifTreeNode;//左线索
 	int rifTreeNode;//右线索
-	char data;//数据
-	TreeNode *lchild;//左孩子
-	TreeNode *rchild;//右孩子
+	char data;
+
+	TreeNode *leftChild;//左孩子
+	TreeNode *rightChild;//右孩子
 	TreeNode *father;//父节点
+
+    TreeNode();
+    TreeNode(char, TreeNode*, TreeNode*, NodeTag, NodeTag);
+    ~TreeNode();
+
 	int tag=0;//是否为空
     int code;
 	void printNode();
 };
 
-// extern TreeNode *nownode;
-// extern TreeNode *Node;
 
-TreeNode *nownode=new TreeNode;
-TreeNode *Node=new TreeNode;
 
-class MyTree{
-    public:
-    TreeNode *Tree;//根节点
+class MyTree
+{
+public:
+    TreeNode *root;//根节点
     int flag;//是否为线索二叉树
-    int num;//结点个数
+    int NodeNum;//结点个数
 	char node[100];//记录元素数组
     MyTree();
     MyTree(const char *a);
@@ -39,7 +52,7 @@ class MyTree{
 	void post(int ileft1,int iright1,int ileft2,int iright2);
     void postOrderTraverse();
     int countLeaf();
-	int height(const TreeNode *Tree);
+	int height(const TreeNode *root);
     int countHeight();
     bool isThreadedTree();
     bool inOrderThreading();
@@ -47,17 +60,18 @@ class MyTree{
     TreeNode& preNode(const TreeNode& n);
     TreeNode& nextNode(const TreeNode& n);
 };
-class HuffmanTree{
-	public:
+class HuffmanTree
+{
+public:
 	struct HTNode{
 		int weight;
 		int parent=-1;
-		int lchild=-1;
-		int rchild=-1;
+		int leftChild=-1;
+		int rightChild=-1;
 	};
 	HTNode *array;
 	int *w;
-	int num;
+	int NodeNum;
 	HuffmanTree(const int& n, const int *a);
 	void printHuffmanCodes();
 };
